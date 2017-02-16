630a11fe36be18b8bde935fe74055897c66aae1a
Ilya Kharchenko <ilya.kharchenko@devexpress.com>
2017-02-16 12:08:16 +0300

    JSDocHelper extracted

---
+++ JSDoc/JSDocHelper.cs
@@ -0,0 +1,61 @@
+﻿using Common;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JSDoc {
+
+    internal class JSDocHelper {
+
+        public IEnumerable<ClassMeta> ParseOutput(string json) {
+            var output = JsonConvert.DeserializeAnonymousType(
+                json,
+                new[] {
+                    new {
+                        // common
+                        Kind = "",
+                        Name = "",
+                        // props
+                        Memberof = "",
+                        Defaultvalue = "",
+                        Type = new {
+                            names = new[] { "" }
+                        }
+                    }
+                }
+            );
+
+            var props = new Dictionary<string, ICollection<PropertyMeta>>();
+            foreach(var propDoc in output.Where(e => e.Kind == "member")) {
+                if(!props.ContainsKey(propDoc.Memberof))
+                    props[propDoc.Memberof] = new List<PropertyMeta>();
+
+                props[propDoc.Memberof].Add(new PropertyMeta(propDoc.Name, propDoc.Defaultvalue, propDoc.Type.names.Select(GetTypeName)));
+            }
+
+            var result = output
+                .Where(e => e.Kind == "class")
+                .Select(c =>
+                    new ClassMeta(
+                        c.Name,
+                        props.ContainsKey(c.Name) ? props[c.Name] : Enumerable.Empty<PropertyMeta>()
+                    )
+                );
+            return result;
+        }
+
+        static string GetTypeName(string type) {
+            if(type.Equals("int", StringComparison.OrdinalIgnoreCase))
+                return "number";
+
+            if(type.Equals("string", StringComparison.OrdinalIgnoreCase))
+                return "string";
+
+            throw new Exception($"Unknown type: '{type}'");
+        }
+    }
+
+}
--- JSDoc/Samples/GenericProp.cs
+++ JSDoc/Samples/GenericProp.cs
@@ -14,55 +14,12 @@ namespace JSDoc.Samples {
 
         public string GetMeta() {
             var jsdoc = new JSDocCommand().Run("samples/GenericProp.js");
-
-            var output = JsonConvert.DeserializeAnonymousType(
-                jsdoc.Output,
-                new[] {
-                    new {
-                        // common
-                        Kind = "",
-                        Name = "",
-                        // props
-                        Memberof = "",
-                        Defaultvalue = "",
-                        Type = new {
-                            names = new[] { "" }
-                        }
-                    }
-                }
-            );
-
-            var props = new Dictionary<string, ICollection<PropertyMeta>>();
-            foreach(var propDoc in output.Where(e => e.Kind == "member")) {
-                if(!props.ContainsKey(propDoc.Memberof))
-                    props[propDoc.Memberof] = new List<PropertyMeta>();
-
-                props[propDoc.Memberof].Add(new PropertyMeta(propDoc.Name, propDoc.Defaultvalue, propDoc.Type.names.Select(GetTypeName)));
-            }
-
-            var result = output
-                .Where(e => e.Kind == "class")
-                .Select(c =>
-                    new ClassMeta(
-                        c.Name,
-                        props.ContainsKey(c.Name) ? props[c.Name] : Enumerable.Empty<PropertyMeta>()
-                    )
-                );
+            var result = new JSDocHelper().ParseOutput(jsdoc.Output);
 
             return JsonConvert.SerializeObject(
                 result,
                 new JsonSerializerSettings { ContractResolver = new LowerCamelCasePropertyNamesContractResolver() }
             );
-        }
-
-        static string GetTypeName(string type) {
-            if(type.Equals("int", StringComparison.OrdinalIgnoreCase))
-                return "number";
-
-            if(type.Equals("string", StringComparison.OrdinalIgnoreCase))
-                return "string";
-
-            throw new Exception($"Unknown type: '{type}'");
         }
     }
 
