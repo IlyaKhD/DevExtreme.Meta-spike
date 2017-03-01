f0eba5d1027c7f9e08cdda023942b5e7b45ecaf6
Ilya Kharchenko <ilya.kharchenko@devexpress.com>
2017-03-01 14:53:46 +0300

    AxisSettings test case added

---
+++ Tests/AxisSettingsTests.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests {
+
+    public class AxisSettingsTests {
+
+        #region Expected
+        const string EXPECTED = @"
+            [
+                {
+                    ""name"": ""PolarAxis"",
+                    ""props"":
+                    [
+                        {
+                            ""name"": ""logarithmBase"",
+                            ""types"": [ ""number"" ],
+                            ""default"": 10
+                        },
+                        {
+                            ""name"": ""label"",
+                            ""types"": [ ""object"" ],
+                            ""default"": null,
+                            ""props"": 
+                            [
+                                {
+                                    ""name"": ""overlappingBehavior"",
+                                    ""types"": [ ""string"" ],
+                                    ""default"": ""enlargeTickInterval""
+                                },
+                                {
+                                    ""name"": ""indentFromAxis"",
+                                    ""types"": [ ""number"" ],
+                                    ""default"": 5
+                                }
+                            ]
+                        }
+                    ],
+                    ""parentType"": null
+                },
+                {
+                    ""name"": ""ChartAxis"",
+                    ""props"":
+                    [
+                        {
+                            ""name"": ""logarithmBase"",
+                            ""types"": [ ""number"" ],
+                            ""default"": 10
+                        },
+                        {
+                            ""name"": ""label"",
+                            ""types"": [ ""object"" ],
+                            ""default"": null,
+                            ""props"": 
+                            [
+                                {
+                                    ""name"": ""alignment"",
+                                    ""types"": [ ""string"" ],
+                                    ""default"": null
+                                },
+                                {
+                                    ""name"": ""indentFromAxis"",
+                                    ""types"": [ ""number"" ],
+                                    ""default"": 5
+                                }
+                            ]
+                        }
+                    ],
+                    ""parentType"": null
+                }
+            ]
+        ";
+        #endregion
+    }
+
+}
